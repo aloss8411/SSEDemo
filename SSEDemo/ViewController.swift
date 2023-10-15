d42578b75ad2fb4233bb8dbae1dc99fc630e816c
Ver <aloss8411@gmail.com>
2023-10-15 13:51:28 +0800

    Initial Commit

---
+++ SSEDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSEDemo
+//
+//  Created by Vergil Wang on 2023/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
