8c160639aba5e7950ed0817a379b0f0912811a93
Ver <aloss8411@gmail.com>
2023-10-15 14:00:35 +0800

    Test for upload file

--- SSEDemo/ViewController.swift
+++ SSEDemo/ViewController.swift
@@ -39,6 +39,8 @@ final class ViewController: UIViewController, ObservableObject {
         setUpTextField()
         setUpBtns()
         setUpTable()
+        
+        print("Test Github")
     }
     
     func setUpTable() {
